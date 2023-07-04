087c656df60e911e1e57c44ed190a99cb1902507
Edwardo Melo <eduardo017melo@gmail.com>
2023-07-04 13:12:32 -0300

    fail

--- src/pages/SavedRecipes.js
+++ src/pages/SavedRecipes.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import { useCookies } from 'react-cookie';
 const SavedRecipes = () => {
   const [savedRecipes, setSavedRecipes] = useState([]);
-
-
+  const [cookies] = useCookies(["access_token"]);
+  const userID = window.localStorage.getItem('userID');
   useEffect(() => {
-    const userID = window.localStorage.getItem('userID');
       const fetchSavedRecipes = async () =>{
        try {
         const response = await axios.get(`http://localhost:3000/recipes/savedRecipes/${userID}`); 
         setSavedRecipes(response.data.savedRecipes);
-        console.log(response.data)
+        //console.log(response.data);
        } catch (error) {
           console.log(error);
        }
@@ -20,8 +19,21 @@ const SavedRecipes = () => {
       }
         fetchSavedRecipes();
 
-}, []);
-  
+}, [savedRecipes]);
+
+  const handleUnsave = async (recipeID) => {
+    try {
+      const response = await axios.delete(`http://localhost:3000/recipes/savedRecipes/${userID}`, {
+        headers: {authorization: cookies.access_token},
+        body: { recipeID } 
+      });
+      //setSavedRecipes(response.data.savedRecipes);
+      console.log(response.data);
+
+    } catch (error) {
+        console.log(error);
+    }
+  }
 
   return (
    <div>
@@ -31,6 +43,7 @@ const SavedRecipes = () => {
         <li key={recipe._id}>
           <div>
             <h2>{recipe.name}</h2>
+            <button onClick={() => handleUnsave(recipe._id)}>unsave</button>
           </div>
           <div className='instructions'>
             <p>{recipe.instructions}</p>
